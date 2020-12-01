d7f6a1666f5b020ca3917bd975238cf5c09b0235
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-30 18:58:16 -0600

    Added String Formatting with Formattable:: AddCommas

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,27 +1,33 @@
 require_relative '../config/environment.rb'
+require_relative './formattable.rb'
 
 class Cli
-    
+    include Formattable::AddCommas
+
     def run
         welcome_user
     end
 
     def welcome_user
         Country.create_from_scraper
+        global_stats = Country.find_global_stats
+        tot_cases = add_commas_to_int("#{global_stats.cases}")
+        tot_deaths = add_commas_to_int("#{global_stats.deaths}")
+        tot_recovered = add_commas_to_int("#{global_stats.recovered}")
         puts ""
         puts "--------------------------------"
         puts "Welcome to COVID-19 Stats Check!"
         puts "--------------------------------"
-        global_stats = Country.find_global_stats
         puts ""
-        puts "TOTAL CASES | #{global_stats.cases}"
+        puts "TOTAL CASES | #{tot_cases}"
         puts ""
-        puts "TOTAL DEATHS | #{global_stats.deaths}"
+        puts "TOTAL DEATHS | #{tot_deaths}"
         puts ""
-        puts "TOTAL RECOVERED | #{global_stats.recovered}"
+        puts "TOTAL RECOVERED | #{tot_recovered}"
         puts ""
         
     end
+
 
 end
 
---
+++ lib/formattable.rb
@@ -0,0 +1,25 @@
+module Formattable
+    module AddCommas
+        def add_commas_to_int(num)
+            split_num = num.to_s.split("")
+            if split_num.length <= 3
+                    split_num.join
+            elsif split_num.length == 4
+                    split_num.insert(1, ",").join
+            elsif split_num.length == 5
+                    split_num.insert(2, ",").join
+            elsif split_num.length == 6 
+                    split_num.insert(3, ",").join
+            elsif split_num.length == 7
+                    split_num.insert(2, ",").insert(5, ",").join
+            elsif split_num.length == 8
+                    split_num.insert(2, ",").insert(6, ",").join
+            elsif split_num.length == 9
+                    split_num.insert(3, ",").insert(7, ",").join
+            elsif split_num.length == 10
+                    split_num.insert(1, ",").insert(4, ",").insert(8, ",").join
+            end
+        end
+    
+    end
+end
