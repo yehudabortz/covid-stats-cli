c20c84c5bb81c54395bafe23298334c0f1db4417
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-30 18:38:50 -0600

    Added Total Cases/Deaths/Recovered

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,16 +1,27 @@
 require_relative '../config/environment.rb'
 
 class Cli
-
-
-    def self.start
-        Cli.new
-        Country.create_from_scraper
-        global_stats = Country.find_global_stats 
-        puts "Welcome to COVID-19 Stats Check!"
-        "Global Coronavirus Cases: #{global_stats.cases}"
-
+    
+    def run
+        welcome_user
     end
 
-binding.pry
+    def welcome_user
+        Country.create_from_scraper
+        puts ""
+        puts "--------------------------------"
+        puts "Welcome to COVID-19 Stats Check!"
+        puts "--------------------------------"
+        global_stats = Country.find_global_stats
+        puts ""
+        puts "TOTAL CASES | #{global_stats.cases}"
+        puts ""
+        puts "TOTAL DEATHS | #{global_stats.deaths}"
+        puts ""
+        puts "TOTAL RECOVERED | #{global_stats.recovered}"
+        puts ""
+        
+    end
+
 end
+
