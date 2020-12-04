c771f272116f958cf658409b36545bcd248081e3
Yehuda Bortz <yehudabortz@icloud.com>
2020-12-04 13:29:25 -0600

    spec modified

--- spec/country_spec.rb
+++ spec/country_spec.rb
@@ -18,20 +18,22 @@ describe CovidStatsCli::Country do
     }
 
     let(:norway) {CovidStatsCli::Country.new(norway_attributes)}
-    
-    context "creates attr_accessor for each key/value pair using #send with a shorter hash" do 
-        it "assigns an attr_accessor upon initialization from a hash as an argument for each key/value pair" do
-            expect(usa.country).to eq(usa_attributes[:country])
-            expect(usa.cases).to eq(usa_attributes[:cases])
+    describe "#initialize" do 
+        context "creates attr_accessor for each key/value pair using #send with a shorter hash" do 
+            it "assigns an attr_accessor upon initialization from a hash as an argument for each key/value pair" do
+                expect(usa.country).to eq(usa_attributes[:country])
+                expect(usa.cases).to eq(usa_attributes[:cases])
+            end
+        end
+        
+        context "creates attr_accessor for each key/value pair using #send with a different longer hash" do 
+            it "also assigns an attr_accessor upon initialization from a hash as an argument for each key/value pair" do
+                expect(norway.country).to eq(norway_attributes[:country])
+                expect(norway.cases).to eq(norway_attributes[:cases])
+                expect(norway.tested).to eq(norway_attributes[:tested])
+            end
         end
     end
-    
-    context "creates attr_accessor for each key/value pair using #send with a different longer hash" do 
-        it "also assigns an attr_accessor upon initialization from a hash as an argument for each key/value pair" do
-            expect(norway.country).to eq(norway_attributes[:country])
-            expect(norway.cases).to eq(norway_attributes[:cases])
-            expect(norway.tested).to eq(norway_attributes[:tested])
-        end
-    end
+
 
 end
