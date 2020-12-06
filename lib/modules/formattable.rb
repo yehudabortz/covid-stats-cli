98257c5e5665d811b6250c1faee460cc07bfc74f
Yehuda Bortz <yehudabortz@icloud.com>
2020-12-06 11:16:35 -0600

    Corrected Number Formatting for Billion

--- lib/modules/formattable.rb
+++ lib/modules/formattable.rb
@@ -17,7 +17,7 @@ module CovidStatsCli::Formattable
             elsif split_num.length == 9
                     split_num.insert(3, ",").insert(7, ",").join
             elsif split_num.length == 10
-                    split_num.insert(1, ",").insert(4, ",").insert(8, ",").join
+                    split_num.insert(1, ",").insert(5, ",").insert(9, ",").join
             end
         end
     end
